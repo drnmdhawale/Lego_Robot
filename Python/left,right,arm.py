33bbc8379741121a0977b248974cf783e48faf3d
Nandkishor Dhawale <107688392+drnmdhawale@users.noreply.github.com>
2022-07-05 18:09:42 +0530

    Update left,right,arm.py
    -u

--- Python/left,right,arm.py
+++ Python/left,right,arm.py
@@ -1,3 +1,13 @@
+# This code was written by Nandkshor Motiram Dhawale, PhD and his intern students on Jan 22nd 2021.
+# This code is written control the motors on the lego ev3 platform
+# The problem statement was given by my self as the R&D Cell Incharge at P.V.P.I.T Budhgaon, Sangli
+
+from pybricks import ev3brick as brick 
+from pybricks.ev3devices import Motor, UltrasonicSensor 
+from pybricks.parameters import Port 
+from pybricks.tools import wait 
+from pybricks.robotics import DriveBase
+
 # Initialize a motor (by default this means clockwise, without any gears). 
 example_motor = Motor(Port.A) #initialize left motor Form Port A
 # Initialize a motor where positive speed values should go counterclockwise 
